f2b62221197075d0a74c28e2639cc1f7e47ff660
Erick Hernández <raider@DESKTOP-LVQHVE6.localdomain>
2026-06-23 05:01:12 -0600

    Run database migrations on backend startup

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,12 +5,15 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.core.config import settings
 from app.db.database import Base
 from app.models.task_model import Task
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
